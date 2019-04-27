3f97bc2426103409e0048137c6830e2fe31ce912
lwatson2 <watson.logan@yahoo.com>
2019-04-27 12:42:16 -0500

    Started styling new post form

--- client/src/components/newpost/NewPost.js
+++ client/src/components/newpost/NewPost.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useForm from "../helpers/FormHelper";
 import validate from "../helpers/NewPostValidationRules";
+import "./NewPost.css";
 
 function NewPost() {
   const handleNewPostSubmit = () => {
@@ -11,37 +12,46 @@ function NewPost() {
     validate
   );
   return (
-    <div>
-      <form>
-        <label className="newPostLabel" htmlFor="title">
-          Title
-        </label>
-        <input
-          value={values.title || ""}
-          placeholder="Please enter the title for your post"
-          onChange={handleChange}
-          className="postTitleInput"
-          type="text"
-          name="title"
-          id="title"
-        />
-        <label htmlFor="gameName">Your game name</label>
-        <input
-          type="text"
-          name="gameName"
-          id="gameName"
-          onChange={handleChange}
-          value={values.gameName}
-        />
-        <label htmlFor="time">Select your start time</label>
-        <input
-          type="time"
-          name="time"
-          id="time"
-          onChange={handleChange}
-          value={values.time || ""}
-        />
-      </form>
+    <div className="newPostPageWrapper">
+      <div className="newPostFormContainer">
+        <form>
+          <div className="newPostGroupWrapper">
+            <label className="newPostLabel" htmlFor="title">
+              Title
+            </label>
+            <input
+              value={values.title || ""}
+              placeholder="Please enter the title for your post"
+              onChange={handleChange}
+              className="postTitleInput"
+              type="text"
+              name="title"
+              id="title"
+            />
+          </div>
+          <div className="newPostGroupWrapper">
+            <label htmlFor="gameName">Your game name</label>
+            <input
+              type="text"
+              name="gameName"
+              id="gameName"
+              onChange={handleChange}
+              value={values.gameName}
+            />
+          </div>
+          <div className="newPostGroupWrapper">
+            <label htmlFor="time">Select your start time</label>
+            <input
+              type="time"
+              name="time"
+              id="time"
+              className="timeInput"
+              onChange={handleChange}
+              value={values.time || ""}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
