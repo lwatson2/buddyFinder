a461c8626af8aa26d6abd9713a2150391132b665
lwatson2 <watson.logan@yahoo.com>
2019-05-06 22:45:08 -0500

    Continued work on post design

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -2,42 +2,91 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Homepage.css";
 
+//xbox color hsl(120, 100%, 47%)
+// Steam color #1b2838
+//Switch color
+
 const Homepage = props => {
+  let system = "Steam";
+
+  const changeSystem = system => {
+    switch (system) {
+      default:
+        return "";
+      case "Playstation":
+        return "Psn";
+      case "Steam":
+        return "Steam";
+      case "Xbox":
+        return "Xbox";
+      case "Switch":
+        return "Switch";
+    }
+  };
+  const changeStyle = system => {
+    switch (system) {
+      default:
+        return "";
+      case "Playstation":
+        return "psnGamertag";
+      case "Steam":
+        return "steamGamertag";
+      case "Xbox":
+        return "xboxGamertag";
+      case "Switch":
+        return "switchGamertag";
+    }
+  };
+  console.log(system);
   return (
     <main className="homepageContainer">
-      <div className="postContainer">
-        <ul className="postList">
-          <li className="postListItem">Lf2 for nightfall</li>
-          <li className="postListItem">
-            {" "}
-            <FontAwesomeIcon icon="gamepad" /> Playstaion{" "}
-          </li>
-          <li className="postListItem">
-            <FontAwesomeIcon icon="gamepad" /> Destiny
-          </li>
-          <li className="postListItem">
-            <FontAwesomeIcon icon="clock" /> 8:45 am
-          </li>
-          <li className="postListItem">
-            <FontAwesomeIcon icon="users" /> 1 / 3 members
-          </li>
-        </ul>
-        <div className="currentMembersContainer">
-          <div className="memberNameContainer">
-            <span className="memberName">Straight</span>
-            <span className="memberGamertag">Psn id: Straightkillinya</span>
-          </div>
-          <div className="memberNameContainer">
-            <span className="memberName">Straight</span>
-            <span className="memberGamertag">Psn id: Straightkillinya</span>
-          </div>
-          <div className="memberNameContainer">
-            <span className="memberName">Straight</span>
-            <span className="memberGamertag">Psn id: Straightkillinya</span>
-          </div>
-          <div className="memberNameContainer">
-            <span className="memberName">Straight</span>
-            <span className="memberGamertag">Psn id: Straightkillinya</span>
+      <div className="postBoxContainer">
+        <div className="postListsContainer">
+          <ul className="postDetailList">
+            <li className="postListItem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              saepe
+            </li>
+            <li className="postListItem">
+              {" "}
+              <FontAwesomeIcon icon="gamepad" /> Playstation{" "}
+            </li>
+            <li className="postListItem">
+              <FontAwesomeIcon icon="gamepad" /> Destiny
+            </li>
+            <li className="postListItem">
+              <FontAwesomeIcon icon="clock" /> 8:45 am
+            </li>
+            <li className="postListItem">
+              <FontAwesomeIcon icon="users" /> 1 / 3 members
+            </li>
+          </ul>
+
+          <div className="currentMembersContainer">
+            <div className="memberNameContainer">
+              <span className="memberName">Straight</span>
+              <span className={changeStyle(system)}>
+                {changeSystem(system)} id: Straightkillinya
+              </span>
+            </div>
+            <div className="memberNameContainer">
+              <span className="memberName">Straight</span>
+              <span className={changeStyle(system)}>
+                {changeSystem(system)} id: Straightkillinya
+              </span>
+            </div>
+            <div className="memberNameContainer">
+              <span className="memberName">Straight</span>
+              <span className={changeStyle(system)}>
+                {changeSystem(system)} id: Straightkillinya
+              </span>
+            </div>
+            <div className="memberNameContainer">
+              <span className="memberName">Straight</span>
+              <span className={changeStyle(system)}>
+                {changeSystem(system)} id: Straightkillinya
+              </span>
+            </div>
           </div>
         </div>
         <button className="joinButton">Join</button>
